6abdd667e7b31cbbf5fa6bcdde007b815b110560
andy10134 <andy10134@hotmail.com>
2020-04-16 17:16:07 -0300

    uwu

--- Punto2/main.py
+++ Punto2/main.py
@@ -8,15 +8,29 @@ def test():
 
     for fila in reader:
         lista.append(V.ViajeroFrecuente(fila[0], fila[1], fila[2], fila[3], fila[4]))
-    archivo.close()
+    archivo.close()    
 
-def punto1():
+def punto1(lista):
     archivo = open('./Punto2/archivopasajeros.csv') 
     reader  = csv.reader(archivo, delimiter=';')
-    lista   = []
+    
     for fila in reader:
         lista.append(V.ViajeroFrecuente(fila[0], fila[1], fila[2], fila[3], fila[4]))
     archivo.close()
 
+def busqueda(lista,idBusqueda):
+    for viajero in lista:
+        if (viajero.cantidadTotaldeMillas() == idBusqueda):
+            return viajero
+    return None
+
 if __name__ == "__main__":
-    punto1()
+    
+    lista   = []
+    
+    punto1(lista)
+    Viajero = busqueda(lista, int(input("Ingrese el id del viajero a buscar: ")))
+
+    if( Viajero != None ):
+        print()
+    #test()
