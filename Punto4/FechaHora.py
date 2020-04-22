a5859be9c96da356bb81f8ced6f297172e5db788
andy10134 <andy10134@hotmail.com>
2020-04-22 11:40:48 -0300

    completo

---
+++ Punto4/FechaHora.py
@@ -0,0 +1,74 @@
+class FechaHora:
+    
+    def __init__(self, dia = 1, mes = 1, anio = 2020, hora = 0, minutos = 0 ,segundos = 0):
+        self.__dia      = dia
+        self.__mes      = mes
+        self.__anio     = anio
+        self.__hora     = hora
+        self.__minutos  = minutos
+        self.__segundos = segundos
+
+    def PonerEnHora(self, hora , minutos = 0, segundos = 0):
+        if ( hora <= 24 and hora > 0 ):
+            self.__hora = hora
+        else: print("Hora invalida")
+        
+        if ( minutos >= 0 and minutos <= 60 ):
+            self.__minutos = minutos
+        else: print("Minutos invalidos")
+
+        if ( segundos >= 0 and segundos <= 60 ):
+            self.__segundos = segundos
+        else: print("Segundos invalidos")
+    
+    def Mostrar(self):
+        print("El dia es " , self.__dia ,"/", self.__mes ,"/",self.__anio , " y la hora es : " , self.__hora , ":" , self.__minutos , ":" , self.__segundos , ".")
+
+    def AdelantarHora(self, horas = 0, minutos = 0):
+        self.__hora    += horas
+        self.__minutos += minutos
+
+        if( (self.__minutos > 60) or (self.__minutos == 60 and self.__segundos > 0) ):
+            self.__minutos = 0
+            self.__hora   += 1
+
+        if( self.__hora > 24 ):
+            dias = self.__hora//24
+            self.setFecha(dias)
+
+
+    def setFecha(self, dia = 0):
+        self.__dia += dia
+        if( self.__dia > self.finMes() ):
+            self.__dia       = 1
+            self.__mes      += 1
+            if( self.__mes >12):
+                print("Feliz año nuevo ;)")
+                self.__mes   = 1
+                self.__anio += 1
+
+    def bisiesto(self):
+        if( self.__anio % 4 == 0 and self.__anio % 100 != 0 or self.__anio % 400 == 0):
+            return 29
+        else:
+            return 28
+
+    def finMes(self):
+        if( self.__mes == 2 ):
+            return self.bisiesto()
+        else:
+            if( self.__mes >8):
+                if( self.__mes % 2 == 0):
+                    return 30
+                else:
+                    return 31
+            else:
+                if( self.__mes % 2 != 0):
+                    return 30
+                else:
+                    return 31
+    
+
+    
+
+    
