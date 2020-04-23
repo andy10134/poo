079b08bf675c6eac5e1465deba23cbbab92ad85d
AnaCarbajo <aniic.1701@gmail.com>
2020-04-23 17:29:08 -0300

    manejador

--- Punto5/ManejadorAlumno.py
+++ Punto5/ManejadorAlumno.py
@@ -3,12 +3,17 @@ import Alumno as A
 
 class ManejadorAlumno:
     def __init__(self):
-        self.__lista= carga()
+        self.__lista= self.carga()
+        print(self.__lista)
         
     
     def carga(self):
         archivo  = open('./Punto5/alumnos.csv')
         reader   = csv.reader(archivo, delimiter=',')
         alumnos  = []
-        for fila in reader
+        for fila in reader:
+            alumnos.append=(A.Alumno(fila[0], fila[1], fila[2], fila[3]))
+        archivo.close()
+        return alumnos
+
 
