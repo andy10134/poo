c5d06649ec5a4064c22970d3f737063361817626
andy10134 <andy10134@hotmail.com>
2020-04-23 16:54:19 -0300

    clase alumno

---
+++ Punto5/Alumno.py
@@ -0,0 +1,19 @@
+
+class Alumno:
+
+    cantidadMaximaDeInasistencias = 20
+    cantidadTotalDeClases = 100
+
+    def __init__(self, nombre, division, anio, cantidadDeInasistencias):
+        self.__nombre = nombre
+        self.__division = division
+        self.__anio = anio
+        self.__cantidadDeInasistencias = cantidadDeInasistencias
+
+    @classmethod
+    def getNombreCantidadMaximaDeInasistencias(cls):
+        return cls.cantidadMaximaDeInasistencias
+    
+    @classmethod
+    def getNombreCantidadTotalDeClases(cls):
+        return cls.cantidadTotalDeClases
