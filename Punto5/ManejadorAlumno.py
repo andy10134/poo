c18c3ad1993e218eba4292f332b48dbc2aad7d59
AnaCarbajo <aniic.1701@gmail.com>
2020-04-23 17:07:52 -0300

    hola

--- Punto5/ManejadorAlumno.py
+++ Punto5/ManejadorAlumno.py
@@ -1,2 +1,14 @@
+import csv
+import Alumno as A
+
 class ManejadorAlumno:
+    def __init__(self):
+        self.__lista= carga()
+        
     
+    def carga(self):
+        archivo  = open('./Punto5/alumnos.csv')
+        reader   = csv.reader(archivo, delimiter=',')
+        alumnos  = []
+        for fila in reader
+
---
+++ Punto5/main.py
@@ -0,0 +1,6 @@
+import csv
+import Alumno as A
+
+if __name__ == "__main__":
+    #test()
+    punto1()
