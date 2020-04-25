f3cda3604536b162fd4f87a046ac374d3d1a7e11
AnaCarbajo <aniic.1701@gmail.com>
2020-04-25 20:22:41 -0300

    listo

--- Punto8/conjunto.py
+++ Punto8/conjunto.py
@@ -1,7 +1,51 @@
+import csv
+
 class Conjunto:
-    def __init__(self, lista):
-        self.__lista = lista
-    
-    def getlista(self):
+    def __init__(self, archivo):
+        self.__lista = []
+        reader   = csv.reader(archivo, delimiter = ',')
+        for fila in reader:
+            self.__lista.append(fila[0])
+        print(self.__lista)
+        
+    def get_lista(self):
         return self.__lista
+
+    def __add__(self, otroConjunto):
+        union= []
+        union.extend(self.__lista)
+        for entero in otroConjunto.__lista:
+            try:
+                i =self.__lista.index(entero)
+            except:
+                i = -1
+            if(i == -1):
+                union.append(entero)
+        return Conjunto(union)
+
+    def __sub__(self, otroConjunto):
+        interseccion= []
+        for entero in otroConjunto.__lista:
+            try:
+                i =self.__lista.index(entero)
+            except:
+                i = -1
+            if(i != -1):
+                interseccion.append(entero)
+        return Conjunto(interseccion)
     
+    def __eq__(self, otroConjunto):
+        bandera=True
+        if(len(self.__lista) == len(otroConjunto.__lista)):
+            i =0
+            while(bandera == True and len(otroConjunto.__lista) != i):
+                try:
+                    indice =self.__lista.index(otroConjunto.__lista[i])
+                    bandera= True
+                except:
+                    bandera= False
+                i += 1
+            if(bandera == True):
+                print("Los conjuntos son iguales :D")
+            else:
+                print("Los conjuntos son distintos D:")
--- Punto8/main.py
+++ Punto8/main.py
@@ -1,22 +1,22 @@
 import csv
-from manejador import ManejadorC
+from conjunto import Conjunto
 
 def opcion0():
     print("Adiós")
 
 def opcion1():
-    print("Código de la opción 1")
+    union= conjunto1 + conjunto2
 
 def opcion2():
-    print("Código de la opción 2")
+    interseccion= conjunto1 - conjunto2
 
 def opcion3():
-    print("Código de la opción 3")
+    igualdad= conjunto1 == conjunto2
 
 switcher = {
     0: opcion0,
-    1: opcion1,
-    2: opcion2,
+    1: opcion1, 
+    2: opcion2, 
     3: opcion3
 }
 
@@ -25,13 +25,19 @@ def switch(argument):
     func()
 
 if __name__ == '__main__':
+    archivo  = open('./Punto8/conjunto1.csv')
+    conjunto1 = Conjunto(archivo)
+    archivo.close()
+    archivo  = open('./Punto8/conjunto2.csv')
+    conjunto2 = Conjunto(archivo)
+    archivo.close()
     bandera = False
     while not bandera:
         print("")
         print("0 Salir")
-        print("1 Opción 1")
-        print("2 Opción 2")
-        print("3 Opción 3")
+        print("1 Union de conjuntos")
+        print("2 Interseccion de conjuntos")
+        print("3 Verificar si los conjuntos son iguales")
         opcion= int(input("Ingrese una opción: "))
         switch(opcion)
-        bandera = int(opcion)==0 
+        bandera = int(opcion) == 0 
