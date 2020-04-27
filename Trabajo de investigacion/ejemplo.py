4eab6dc82dd673aa490a5ccaaf060cf023d4255f
AnaCarbajo <aniic.1701@gmail.com>
2020-04-27 19:02:14 -0300

    gegbdj

---
+++ Trabajo de investigacion/ejemplo.py
@@ -0,0 +1,23 @@
+class Persona:
+    __nombre    = ''
+    __dni       = ''
+
+    def __init__(self, nombre= '', dni= '', domicilio= ''):
+        self.__nombre       = nombre
+        self.__dni          = dni
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def getDni(self):
+        return self.__dni
+    
+    def setNombre(self, nombre):
+        self.__nombre = nombre
+    
+    def setDni(self, dni):
+        self.__dni = dni
+    
+    def repr(self):
+        return str(self)
+        
--- Trabajo de investigacion/mainejemplo.py
+++ Trabajo de investigacion/mainejemplo.py
@@ -1,7 +1,12 @@
-from persona import Persona
+from ejemplo import Persona
 
 if __name__ == '__main__':
     lista= []
     nombre= input("Ingrese nombre: ")
     dni = input("Ingrese dni: ")
     lista.append(Persona(nombre, dni))
+    lista2= []
+    lista2.append(Persona(nombre, dni))
+    lista2.append(Persona(nombre, dni))
+    lista.extend(lista2)
+    lista.insert(2, Persona('Mario', '21345627'))
