087466515e558319e32aea2791fe65531b10d5df
andy10134 <andy10134@hotmail.com>
2020-04-27 19:26:02 -0300

    .

--- Trabajo de investigacion/mainejemplo.py
+++ Trabajo de investigacion/mainejemplo.py
@@ -1,13 +1,18 @@
 from ejemplo import Persona
 
 if __name__ == '__main__':
-    lista2 = []
-    
-    lista2.append(Persona('Mariana', '45666743'))
-    lista2.append(Persona('Jorge', '42356255'))
+    lista= []
+    lista2= []
+
+    lista.append(Persona('Mariana', '45666743'))
+
+    lista2.append(Persona('Jorge','42356255'))
     lista2.append(Persona('Ana', '43488985'))
-    
-    nuevo =Persona('Jorge', 42356255)
+
+    lista.extend(lista2)
 
     print(lista2)
 
+    lista2.pop()
+
+    print(lista2)
