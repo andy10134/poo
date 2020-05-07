6c4fc6836fe1c0295c4e9d2c59e665d132e09154
AnaCarbajo <aniic.1701@gmail.com>
2020-05-07 17:54:42 -0300

    libro

---
+++ practica2/Punto1/libro.py
@@ -0,0 +1,19 @@
+
+class Libro: 
+    __idLibro            = 0
+    __titulo             = ""
+    __autor              = ""
+    __editorial          = ""
+    __isbn               = 0
+    __cantidadCapitulos  = 0
+    __capitulos          = []
+    def __init__(self, idLibro, titulo, autor, editorial, isbn, capitulos):
+        self.__idLibro= idLibro
+        self.__titulo= titulo
+        self.__autor= autor
+        self.__editorial= editorial
+        self.__isbn = isbn
+        self.__cantidadCapitulos= capitulos
+    
+    def agregarcapitulo(self, capitulo):
+        self.__capitulos.append()
