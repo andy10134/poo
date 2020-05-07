e342caa367c56835aa80d3c106ded373f636c744
andy10134 <andy10134@hotmail.com>
2020-05-07 18:10:14 -0300

    clase capitulo terminada

--- practica2/Punto1/Capitulo.py
+++ practica2/Punto1/Capitulo.py
@@ -6,3 +6,15 @@ class Capitulo:
     def __init__(self, titulo, cantidadPaginas):
         __titulo = titulo
         __cantidadPaginas = cantidadPaginas
+
+    def getTitulo(self):
+        return self.__titulo
+    
+    def getCantidadPaginas(self):
+        return self.__cantidadPaginas
+
+    def setTitulo(self, titulo):
+        self.__titulo = titulo
+    
+    def setCantidadPaginas(self, cantidadPaginas):
+        self.__cantidadPaginas = cantidadPaginas
