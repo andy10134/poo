831687b71e079e8e2b77be80c5ccc1620fb75372
AnaCarbajo <aniic.1701@gmail.com>
2020-05-12 21:36:14 -0300

    listo

--- practica2/Punto1/main.py
+++ practica2/Punto1/main.py
@@ -20,10 +20,11 @@ def carga(manejadorLibro):
 
 
 def opcion0():
-    print("Hasta la proximaaaaa *dubstep de fondo*")
+    print("Adios")
 
 def opcion2():
-    pass
+    palabra= input('Ingrese palabra a buscar: ')
+    manejadorLibro.buscarPalabra(palabra)
 
 def opcion1():
     idlibro=int(input('Ingrese id del libro: '))
@@ -48,22 +49,8 @@ if __name__ == "__main__":
     while not bandera:
         print("")
         print("0 Salir")
-        print("1 Liste nombre y porcentaje de inasistencias de los alumnos")
-        print("2 Modificar la cantidad máxima de inasistencias permitidas.")
+        print("1 Ingresar el identificador de un libro y mostrar título del libro, nombre de cada uno de sus capítulos y cantidad total de páginas de un libro")
+        print("2 Dada una palabra, mostrar título y autor de los libros que contienen la palabra dada en su título o en el título de alguno de sus capítulos")
         opcion = int(input("Ingrese una opción: "))
         switch(opcion)
         bandera = int(opcion) == 0 
-
-
-#Implemente un programa principal que permita:
-#
-#a. Cargar los datos de los libros en una instancia de la clase ManejaLibros.
-# Para esto debe considerar que la biblioteca ha provisto un archivo libros.csv con los datos de los libros. 
-# Este archivo presenta la siguiente estructura lógica: en una línea están los datos del libro y a continuación, 
-#una línea por cada capítulo con sus respectivos datos. Esto se repite para cada libro. A continuación se da un ejemplo.
-
-#b.     A través de un menú de opciones cumplir con las siguientes funcionalidades:
-#1. Ingresar el identificador de un libro y mostrar título del libro, nombre de cada uno de sus capítulos y
-# cantidad total de páginas de un libro.
-#2.   Dada una palabra, mostrar título y autor de los libros que contienen la palabra dada en su título o en el
-# título de alguno de sus capítulos.
