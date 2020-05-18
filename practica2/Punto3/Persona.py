0d23529260c8efc16166359481c5909a54cb7d32
AnaCarbajo <aniic.1701@gmail.com>
2020-05-18 18:18:07 -0300

    persona

--- practica2/Punto3/Persona.py
+++ practica2/Punto3/Persona.py
@@ -8,3 +8,15 @@ class Persona:
         self.__nombre = nombre
         self.__direccion = direccion
         self.__dni = dni
+
+    def getNombre(self):
+        return self.__nombre
+
+    def getDireccion(self):
+        return self.__direccion
+
+    def getDni(self):
+        return self.__dni
+
+    def __str__(self):
+        return ('Nombre: '+ self.getNombre +'Dirección: '+ self.getDireccion +'Dni: '+ self.getDni)
