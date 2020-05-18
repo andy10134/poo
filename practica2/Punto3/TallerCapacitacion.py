b4a6c38a3413601953abe7da198e05d9470c18da
AnaCarbajo <aniic.1701@gmail.com>
2020-05-18 18:33:45 -0300

    taller

--- practica2/Punto3/TallerCapacitacion.py
+++ practica2/Punto3/TallerCapacitacion.py
@@ -12,3 +12,27 @@ class TallerCapacitacion:
         self.__vacantes = vacantes
         self.__montoInscripcion = montoInscripcion
         self.__inscripciones = []
+    
+    def agregarInscripcion(self, inscripcion):
+        self.__inscripciones.append(inscripcion)
+
+    def getId(self):
+        return self.__idTaller
+
+    def getNombre(self):
+        return self.__nombre
+
+    def getVacantes(self):
+        return self.__vacantes
+
+    def getMonto(self):
+        return self.__montoInscripcion
+
+    def getInscripciones(self):
+        return self.__inscripciones
+
+    def setVacantes(self, numero):
+        self.__vacantes-= 1
+
+    
+
