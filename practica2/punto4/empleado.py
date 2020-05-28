8d0abca65e539f09dca93650b3baf64d6fbd65d0
AnaCarbajo <aniic.1701@gmail.com>
2020-05-28 17:31:16 -0300

    dxcvbn

---
+++ practica2/punto4/empleado.py
@@ -0,0 +1,31 @@
+
+class Empleado():
+
+    __dni = 0
+    __nombre = None
+    __direccion = None
+    __telefono = 0
+
+    def __init__(self, dni, nombre, direccion, telefono):
+        self.__dni = dni
+        self.__nombre = nombre
+        self.__direccion = direccion
+        self.__telefono = telefono
+    
+    def getDni(self):
+        return self.__dni
+    
+    def getNombre(self):
+        return self.__nombre
+
+    def getDireccion(self):
+        return self.__direccion
+
+    def getTelefono(self):
+        return self.__telefono
+
+    def __str__(self):
+        return (('DNI: {} \nNombre: {} \nDireccion: {} \nTelefono: {}').format(
+            self.getDni(), self.getNombre(), self.getDireccion(),
+            self.getTelefono()
+            ))
---
+++ practica2/punto4/empleadoPlanta.py
@@ -0,0 +1,16 @@
+from empleado import Empleado
+
+class EmpleadoPlanta(Empleado):
+
+    __sueldoBasico = 0
+    __antiguedad = 0
+
+    def __init__(self, sueldo, antiguedad):
+        self.__sueldoBasico = sueldo
+        self.__antiguedad = antiguedad
+
+    def getSueldo(self):
+        return self.__sueldoBasico
+
+    def getAntiguedad(self):
+        return self.__antiguedad
