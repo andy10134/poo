611f1d60868005efc56c52c217b6d2a3473f3613
AnaCarbajo <aniic.1701@gmail.com>
2020-05-28 18:48:37 -0300

    sdhh

---
+++ practica2/punto4/manejadorEmpleado.py
@@ -0,0 +1,16 @@
+from empleado import Empleado
+from empleadoPlanta import EmpleadoPlanta
+from empleadosExternos import E
+from empleadoContratado import EmpleadoContratado
+import numpy as np
+
+class ManejadorEmpleado:
+
+    __cantidad = 0
+
+    def __init__(self, cantidad):
+        self.__cantidad =  cantidad
+        self.__arreglo = np.empty(self.__cantidad, dtype= Empleado)
+
+    def agregarEmpleado(self, ):
+        pass
