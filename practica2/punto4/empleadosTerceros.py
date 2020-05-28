ea357bc402d57dbbe4dbe0741957a065877d8287
andy10134 <andy10134@hotmail.com>
2020-05-28 17:50:20 -0300

    .

--- practica2/punto4/empleadosTerceros.py
+++ practica2/punto4/empleadosTerceros.py
@@ -1,7 +1,13 @@
-class EmpleadosTerceros(Empleados):
+from empleado import Empleado
+
+
+class EmpleadoTercero(Empleado):
 
     fechaInicio = ""
     fechaFinalizacion = ""
-    
-    def __init__(self):
-        pass
+
+    def __init__(self, dni, nombre, direccion,
+                 telefono, fecha_inicio, fecha_fin):
+        self.fechaInicio = fecha_inicio
+        self.fechaFinalizacion = fecha_fin
+        super().__init__(dni, nombre, direccion, telefono)
