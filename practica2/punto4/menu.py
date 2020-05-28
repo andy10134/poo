c6d184e84fe3f543547f7cf3c32a10a1dd0ebf33
andy10134 <andy10134@hotmail.com>
2020-05-28 18:45:08 -0300

    .

---
+++ practica2/punto4/menu.py
@@ -0,0 +1,28 @@
+import os
+
+
+class Menu:
+
+    __switcher = None
+
+    def __init__(self):
+        self.__switcher = {
+            1: self.opcion1,
+            2: self.opcion2,
+            3: self.opcion3,
+            4: self.opcion4,
+            5: self.opcion5,
+            0: self.salir
+        }
+        self.__empleados = None
+
+    def getSwitcher(self):
+        return self.__switcher
+
+    def opcion(self, op):
+        os.system("clear")
+        func = self.__switcher.get(op, lambda: print("Opción no válida"))
+        func()
+
+    def salir(self):
+        print('Salir')
