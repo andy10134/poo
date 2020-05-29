b67b4b9fec34a4eb1cfe6445bf6936e2f4c4f8de
andy10134 <andy10134@hotmail.com>
2020-05-29 13:12:06 -0300

    .

--- practica2/punto4/manejadorEmpleado.py
+++ practica2/punto4/manejadorEmpleado.py
@@ -23,31 +23,34 @@ class ManejadorEmpleado:
         archivo = open('./planta.csv')
         reader = csv.reader(archivo, delimiter=(','))
         for fila in reader:
-            self.__arreglo[self.__cantidad] = EmpleadoPlanta(
-                fila[0], fila[1], fila[2], fila[3], fila[4], fila[5]
-            )
-            self.__cantidad += 1
+            if(self.__cantidad < self.__dimension):
+                self.__arreglo[self.__cantidad] = EmpleadoPlanta(
+                    fila[0], fila[1], fila[2], fila[3], fila[4], fila[5]
+                )
+                self.__cantidad += 1
         archivo.close()
 
     def agregarEmpleadoContratado(self):
         archivo = open('./contratados.csv')
         reader = csv.reader(archivo, delimiter=(','))
         for fila in reader:
-            self.__arreglo[self.__cantidad] = EmpleadoContratado(
-                fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6]
-            )
-            self.__cantidad += 1
+            if(self.__cantidad < self.__dimension):
+                self.__arreglo[self.__cantidad] = EmpleadoContratado(
+                    fila[0], fila[1], fila[2], fila[3], fila[4], fila[5], fila[6]
+                )
+                self.__cantidad += 1
         archivo.close()
 
     def agregarEmpleadoExterno(self):
         archivo = open('./externos.csv')
         reader = csv.reader(archivo, delimiter=(','))
         for fila in reader:
-            self.__arreglo[self.__cantidad] = EmpleadosExternos(
-                fila[0], fila[1], fila[2], fila[3], fila[4],
-                fila[5], fila[6], fila[7], fila[8], fila[9]
-            )
-            self.__cantidad += 1
+            if(self.__cantidad < self.__dimension):
+                self.__arreglo[self.__cantidad] = EmpleadosExternos(
+                    fila[0], fila[1], fila[2], fila[3], fila[4],
+                    fila[5], fila[6], fila[7], fila[8], fila[9]
+                )
+                self.__cantidad += 1
         archivo.close()
     
     def buscarTarea(self, tarea):
