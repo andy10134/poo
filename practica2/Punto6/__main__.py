7bc4b89b70c4bb15255be7fa40147f359e8a4e76
andy10134 <andy10134@hotmail.com>
2020-06-01 20:32:57 -0300

    .

--- practica2/Punto6/__main__.py
+++ practica2/Punto6/__main__.py
@@ -1,4 +1,19 @@
 from menu import Menu
 
 if __name__ == "__main__":
-    pass
+    menu = Menu()
+    salir = False
+    print("MiAuto.com")
+    while not salir:
+        print("=====================================")
+        print("0 Salir")
+        print("1-Insertar un vehículo en la colección en una posición determinada.")
+        print("2-Agregar un vehículo a la colección.")
+        print("3-Dada una posición de la Lista: Mostrar por pantalla qué tipo de objeto se encuentra almacenado en dicha posición.")
+        print("4-Dada la patente de un vehículo usado, modificar el precio base, y luego mostrar el precio de venta.")
+        print("5-Mostrar todos los datos, incluido el importe de venta, del vehículo más económico.")
+        print("6-Mostrar para todos los vehículos que la concesionaria tiene a la venta, modelo, cantidad de puertas e importe de venta.")
+        print("7-Almacenar los objetos de la colección Lista en el archivo “vehiculos.json”.")
+        opcion = int(input("Ingrese una opción: "))
+        menu.opcion(opcion)
+        salir = int(opcion) == 0
