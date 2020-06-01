b56b3f395f046fd52cb7be964df5cc74e1613f8c
AnaCarbajo <aniic.1701@gmail.com>
2020-06-01 19:30:31 -0300

    adds

---
+++ practica2/Punto6/auto.py
@@ -0,0 +1,24 @@
+
+class Auto():
+    __modelo = None
+    __cantidadPuertas = 0
+    __color = None
+    __precioBase = 0
+
+    def __init__(self, modelo, puertas, color, precio):
+        self.__modelo = modelo
+        self.__cantidadPuertas = int(puertas)
+        self.__color = color
+        self.__precioBase = precio
+    
+    def getModelo(self):
+        pass
+
+    def getCantidadPuertas(self):
+        pass
+
+    def getColor(self):
+        pass
+
+    def getPrecioBase(self):
+        pass
