ceb8ff18671b2729f80bcf9fe1b881aa4c3c614c
andy10134 <andy10134@hotmail.com>
2020-06-01 14:13:18 -0300

    .

---
+++ practica2/Punto5/interface.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+
+class IManejardor(Interface):
+    
+    def insertarElemento(self):
+        pass
+
+    def agregarElemento(self):
+        pass
+
+    def mostrarElemento(self):
+        pass
