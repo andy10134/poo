0565f67347fd2a60364248aaee751870cb03428b
andy10134 <andy10134@hotmail.com>
2020-06-01 19:44:46 -0300

    .

---
+++ practica2/Punto6/autoNuevo.py
@@ -0,0 +1,13 @@
+from auto import Auto
+
+class autoNuevo(Auto):
+
+    __version = ""
+
+    def __init__(self, modelo, puertas, color, precio, version):
+        self.__version = version
+
+        super().__init__(modelo, puertas, color, precio)
+    
+    def getVersion(self):
+        return self.__version
