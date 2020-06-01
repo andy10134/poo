529acee103d44382088cf25b6a2e05c01a55af45
AnaCarbajo <aniic.1701@gmail.com>
2020-06-01 17:59:11 -0300

    cffvgh

--- practica2/Punto5/interface.py
+++ practica2/Punto5/interface.py
@@ -2,5 +2,12 @@ from zope.interface import Interface
 from zope.interface import implementer
 
 class Iinterface(Interface):
+
     def insertarElemento(self, elemento, indice):
+        pass
+
+    def agregarElemento(self, elemento):
+        pass
+
+    def mostrarElemento(self, elemento):
         pass
