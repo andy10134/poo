3b1f0a8ecae031f0d3269c410f21d0fc7b895e79
andy10134 <andy10134@hotmail.com>
2020-06-04 11:36:13 -0300

    .

---
+++ practica2/Punto7/models/persona.py
@@ -0,0 +1,29 @@
+class Persona:
+
+    __cuil = ""
+    __nombre = ""
+    __apellido = ""
+    __sueldo = 0
+    __antiguiedad = 0
+
+    def __init__(self, cuil, nombre, apellido, sueldo, antiguiedad):
+        self.__cuil = cuil
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__sueldo = sueldo
+        self.__antiguiedad = antiguiedad        
+
+    def getCuil(self):
+        return self.__cuil
+    
+    def getNombre(self):
+        return self.__nombre
+
+    def getApellido(self):
+        return self.__apellido
+    
+    def getSueldo(self):
+        return self.__sueldo
+    
+    def getAntiguiedad(self):
+        return self.__antiguiedad
