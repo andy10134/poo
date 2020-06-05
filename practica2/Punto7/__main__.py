7555f42963923e7d089748298f98cd8e9e5c9b2e
AnaCarbajo <aniic.1701@gmail.com>
2020-06-04 21:52:23 -0300

    .

--- practica2/Punto7/__main__.py
+++ practica2/Punto7/__main__.py
@@ -1,29 +1,35 @@
-# Narrativa          
-# 
-# De todo el personal se conoce la siguiente información: cuil, apellido, nombre, sueldo básico y antigüedad.
-# 
-# Si es un docente se registra además carrera en la que dicta clases, cargo y cátedra.
-# 
-# Si es personal de apoyo se registra además categoría.
-# 
-# Si es un investigador se registra además área de investigación y tipo de investigación
-# 
-# Si es un docente investigador se registra además carrera en la que dicta clases, cargo, cátedra, área de investigación y tipo de investigación, categoría en el programa de incentivos de investigación, importe extra por docencia e investigación.
-# 
-# Para cumplir con las tareas solicitadas por el analista, usted deberá:
-# 
-# a) Definir la jerarquía de clases correspondiente a la narrativa dada.
-# 
-# b) Almacenar en una colección tipo Lista definida por el programador, los agentes de la Universidad, obteniendo los datos del archivo “personal.json”, la misma deberá implementar la interfaz definida en el ejercicio 5. Los nodos de la lista, serán referencias a objetos que representen objetos de la clase base de la jerarquía.    
-# 
-# c) Implementar un programa principal con un menú de opciones que permita testear las siguientes acciones:
-# 
-#         Insertar a agentes a la colección.
-#         Agregar agentes a la colección.
-#          Dada una posición de la lista: Mostrar por pantalla que tipo de agente se encuentra almacenado en dicha posición.
-#         Ingresar por teclado el nombre de una carrera y generar un listado ordenado por nombre con todos los datos de los agentes que se desempeñan como docentes investigadores.
-#          Dada un área de investigación, contar la cantidad de agentes que son docente    investigador, y la cantidad de investigadores que trabajen en ese área.
-#         Recorrer la colección y generar un listado que muestre nombre y apellido, tipo de Agente y sueldo de todos los agentes, ordenado por apellido.
-#         Dada una categoría de investigación (I, II, III, IV o V), leída desde teclado, listar apellido, nombre e importe extra por docencia e investigación, de todos los docentes investigadores que poseen esa categoría, al final del listado deberá mostrar el total de dinero que la Secretaría de Investigación debe solicitar al Ministerio en concepto de importe extra que cobran los docentes investigadores de la categoría solicitada.    
-#         Almacenar los datos de todos los agentes en el archivo “personal.json”    
-# 
+from menu import Menu
+
+if __name__ == "__main__":
+    menu = Menu()
+    salir = False
+    print("App Centro de Cómputos UNSJ")
+    while not salir:
+        print("=====================================")
+        print("0 Salir")
+        print("1-Insertar a agentes a la colección.")
+        print("2-Agregar agentes a la colección.")
+        print(
+            "3-Dada una posición de la Lista: Mostrar por pantalla qué tipo",
+            " de agente se encuentra almacenado en dicha posición.")
+        print(
+            "4-Ingresar por teclado el nombre de una carrera y generar un", 
+            "listado ordenado por nombre con todos los datos de los agentes",
+            " que se desempeñan como docentes investigadores.")
+        print(
+            "5-Dada un área de investigación, contar la cantidad de agentes",
+            " que son docente investigador, y la cantidad de investigadores",
+            " que trabajen en ese área.")
+        print(
+            "6-Recorrer la colección y generar un listado que muestre nombre",
+            " y apellido, tipo de Agente y sueldo de todos los agentes,",
+            " ordenado por apellido.")
+        print(
+            "7-Dada una categoría de investigación (I, II, III, IV o V),",
+            " listar apellido, nombre e importe extra, por docencia e investigación",
+            " de todos los docentes investigadores que poseen esa categoría")
+        print(
+            "8-Almacenar los datos de todos los agentes en el archivo “personal.json”.")
+        opcion = int(input("Ingrese una opción: "))
+        menu.opcion(opcion)
+        salir = int(opcion) == 0
---
+++ practica2/Punto7/menu.py
@@ -0,0 +1,69 @@
+import os
+from objectEncoder import ObjectEncoder
+from lista import Lista
+
+
+class Menu:
+
+    __switcher = None
+
+    def __init__(self):
+        os.system("cls")
+        self.__switcher = {
+            1: self.opcion1,
+            2: self.opcion2,
+            3: self.opcion3,
+            4: self.opcion4,
+            5: self.opcion5,
+            6: self.opcion6,
+            7: self.opcion7,
+            0: self.salir
+        }
+        self.__jsonF = ObjectEncoder()
+        self.__lista = Lista()
+
+    def getSwitcher(self):
+        return self.__switcher
+
+    def opcion(self, op):
+        os.system("cls")
+        func = self.__switcher.get(op, lambda: print("Opción no válida"))
+        func()
+
+    def opcion1(self):
+        pass
+    #    datos = self.ingresarDatos()
+    #    if(type(datos) is not None):
+    #        posicion = int(input("Ingrese la posición en la "
+    #                             "cual quiere insertar el vehiculo: "))
+    #        self.__lista.agregarVehiculo(datos, posicion)
+
+    def opcion2(self):
+        pass
+    #    datos = self.ingresarDatos()
+    #    if(type(datos) is not None):
+    #        self.__lista.agregarVehiculo(datos)
+
+    def opcion3(self):
+        pass
+    #    posicion = int(input('Ingrese posicion: '))
+    #    self.__lista.mostrarElemento(posicion-1)
+
+    def opcion4(self):
+        pass
+
+    def opcion5(self):
+        pass
+
+    def opcion6(self):
+        pass
+
+    def opcion7(self):
+        pass
+
+    def opcion8(self):
+        d = self.__lista.toJSON()
+        self.__jsonF.guardarJSONArchivo(d, 'personal.json')
+
+    def salir(self):
+        print('Salir')
