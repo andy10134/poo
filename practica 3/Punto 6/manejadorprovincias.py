76be949501ae426b470b24a6eac66112ebbe7f9e
AnaCarbajo <aniic.1701@gmail.com>
2020-07-16 17:37:57 -0300

    .

---
+++ practica 3/Punto 6/manejadorprovincias.py
@@ -0,0 +1,38 @@
+from provincia import Provincia
+
+class ManejadorProvincias():
+
+    indice = 0
+    __provincias = None
+
+    def __init__(self):
+        self.__provincias = []
+
+    def agregarProvincia(self, provincia):
+        provincia.rowid = ManejadorProvincias.indice
+        ManejadorProvincias.indice+=1
+        self.__provincias.append(provincia)
+
+    def getLista(self):
+        return self.__provincias
+    
+    def eliminarProvincia(self, provincia):
+        indice = self.obtenerIndice(provincia)
+        self.__provincias.pop(indice)
+    
+    def obtenerIndice(self, provincia):
+        bandera = False
+        i=0
+        while not bandera and i < len(self.__provincias):
+            if self.__provincias[i].rowid == provincia.rowid:
+                bandera=True
+            else:
+                i+=1
+        return i
+
+    def toJSON(self):
+        d = dict(
+            __class__ = self.__class__.__name__,
+            provincias = [provincia.toJSON() for provincia in self.__provincias]
+        )
+        return d
