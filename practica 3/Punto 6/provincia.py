c6e0c31fc62e1e21fa3a8ef7ce6d7b364fff5a5f
AnaCarbajo <aniic.1701@gmail.com>
2020-07-16 17:30:41 -0300

    .

---
+++ practica 3/Punto 6/provincia.py
@@ -0,0 +1,42 @@
+
+class Provincia():
+
+    __nombre = ''
+    __capital = ''
+    __habitantes = 0
+    __departamentos = 0
+
+    def __init__(self, nombre, capital, habitantes, departamentos):
+        self.__nombre = self.requerido(nombre, "Nombre es un valor requerido")
+        self.__capital = self.requerido(capital, "Capital es un valor requerido")
+        self.__habitantes = self.requerido(habitantes, "Cantidad de habitantes es un valor requerido")
+        self.__departamentos = self.requerido(departamentos, "Cantidad de departamentos es un valor requerido")
+    
+    def requerido(self, valor, mensaje):
+        if not valor:
+            raise ValueError(mensaje)
+        return valor
+
+    def getNombre(self):
+        return self.__nombre
+    
+    def getCapìtal(self):
+        return self.__capital
+    
+    def getHabitantes(self):
+        return self.__habitantes
+
+    def getDepartamentos(self):
+        return self.__departamentos
+    
+    def toJSON(self):
+        d = dict(
+            __class__ = self.__class__.__name__,
+            __atributos__ = dict(
+                nombre = self.__nombre,
+                capital = self.__capital,
+                habitantes = self.__habitantes,
+                departamentos = self.__departamentos
+            )
+        )
+        return d
