10075f1ff7c3c318b94629284c224cb9fe00cf3c
AnaCarbajo <aniic.1701@gmail.com>
2020-07-16 18:06:51 -0300

    .

--- practica 3/Punto 6/repositorio.py
+++ practica 3/Punto 6/repositorio.py
@@ -1,8 +1,8 @@
-from paciente import Paciente
-from manejadorPaciente import ManejadorPaciente
+from provincia import Provincia
+from manejadorprovincias import ManejadorProvincias
 from objectEncoder import ObjectEncoder
 
-class RespositorioPacientes:
+class RespositorioProvincias:
     __pac = None
     __manejador = None
 
@@ -11,26 +11,15 @@ class RespositorioPacientes:
         diccionario = self.__pac.leerJSONArchivo()
         self.__manejador = self.__pac.decodificarDiccionario(diccionario)
     
-    def to_values(self, paciente):
-        return paciente.getApellido(), paciente.getNombre(), paciente.getEmail(), paciente.getTelefono()
+    def to_values(self, provincia):
+        return provincia.getNombre(), provincia.getCapital(), provincia.getHabitantes(), provincia.getDepartamentos()
     
-    def obtenerListaPacientes(self):
+    def obtenerListaProvincias(self):
         return self.__manejador.getLista()
     
-    def agregarPaciente(self, paciente):
-        self.__manejador.agregarPaciente(paciente)
-        return paciente
-    
-    def modificarPaciente(self, paciente):
-        self.__manejador.actualizarPaciente(paciente)
-        return paciente
-    
-    def borrarPaciente(self, paciente):
-        self.__manejador.eliminarPaciente(paciente)
-    
-    def calcularIMC(self, paciente):
-        imc = self.__manejador.calcularIMC(paciente)
-        return imc
+    def agregarProvincia(self, provincia):
+        self.__manejador.agregarProvincia(provincia)
+        return provincia
 
     def grabarDatos(self):
         self.__pac.guardarJSONArchivo(self.__manejador.toJSON())
