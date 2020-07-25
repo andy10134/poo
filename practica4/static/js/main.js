c5b7ddbdef6ec7304205f750e3cd6b43df0dcf5e
andy10134 <andy10134@hotmail.com>
2020-07-24 21:10:01 -0300

    .

--- practica4/static/js/main.js
+++ practica4/static/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    
+    $('.product-item').click(function (e) { 
+
+        if($('.order-list:last-child').hasClass('product-item')){
+            $('.order-list:last-child').remove();
+        }
+        $(this).clone().appendTo('.order-list');
+
+        
+    });
 });
