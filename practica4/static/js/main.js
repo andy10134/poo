cc43b3cf2fc76882f916311ecbcadb677728fdc0
Andy Molina <andy10134@hotmail.com>
2020-07-28 17:11:59 -0300

    .

--- practica4/static/js/main.js
+++ practica4/static/js/main.js
@@ -29,4 +29,28 @@ $(document).ready(function () {
         $('#final-price').text($('#total').val());
        
     });
+
+    $(".pendiente-item").click(function (e) { 
+        e.preventDefault();
+        
+        var titulo = $(this).children().children('.mb-1').text();
+        titulo += "-"
+        titulo += $(this).children().children('.text-muted').text();
+        $('#tituloModal').text(titulo);        
+        
+        $('.modal-body').empty();
+
+        var pedidos = $(this).children('p').clone().appendTo($('.modal-body'));
+        
+        pedidos.each(function() {
+            if($( this ).hasClass( "pedido-Pendiente" )){
+                icon = '<span style="font-size: 20px;" class="material-icons aling-icon">access_time</span>';
+            } else{
+                icon = '<span style="font-size: 20px;" class="material-icons aling-icon; background: #2fb191;">check</span>';                
+            }
+            $(this).prepend(icon);
+          })
+
+        console.log(pedidos)
+    });
 });
